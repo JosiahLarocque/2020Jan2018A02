182caae5b963eb9bfa10f665950052d7f0ac770d
Josiah Larocque <jlarocque13@nait.ca>
2020-01-10 16:43:33 -0700

    .

---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+#endregion
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        //basic complete list of DBSet
+        public List<Artist> Artist_List()
+        {
+            //set up the code block to ensure the release of the sql connection
+            using (var context = new ChinookContext())
+            {
+                //.ToList<T> is used to convert the DbSet<T> into a List<T> collection
+                return context.Artists.ToList();
+            }
+        }
+
+        //basic query: return a record based on pkey
+        public Artist Artist_FindByID(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using System.Data.Entity;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+
+    internal class ChinookContext:DbContext
+    {
+        //constructor will pass the connection string name to the database
+        //      to Entityframework via DbContext
+        public ChinookContext():base("ChinookDB")
+        {
+
+        }
+                
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album>  Albums { get; set; }
+
+    }
+   
+}
